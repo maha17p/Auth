3668f921e348c8b105701063b9ca9dcfd259b922
Maharaja <108454691+maha17p@users.noreply.github.com>
2023-08-27 07:19:15 +0000

    basic folder structure added

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// import package
 import express from "express";
+import "dotenv"
 
+
+// import files
+
+
+
+// express invoke
 const app = express();
 const PORT_NUM = 5000;
+
+// use
+
 
 app.listen(PORT_NUM, () => console.log(`Server is listening to ${PORT_NUM}`));
